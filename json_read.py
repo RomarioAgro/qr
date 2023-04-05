65cf4d6120a1ee634da5782a23201045ce5e0dae
Кожин Роман <resume-gl@yandex.ru>
2023-04-05 10:45:08 +0300

    мкласс чтения json файла

--- json_read.py
+++ json_read.py
@@ -1,7 +1,20 @@
 import json
 
 
-with open('qr.json') as json_file:
-    data = json.load(json_file)
-    for pt in data['price_tag']:
-        print(pt['qr'])
+class ReadJSON:
+    """
+    класс чтения json файла с нашими ценниками
+    """
+    def __init__(self, f_path: str = 'd:\\files\\', f_name: str = 'qr.json'):
+        with open(f_path + f_name) as json_file:
+            self.data = json.load(json_file)
+
+
+
+def main():
+    i_data = ReadJSON(f_path='d:\\files\\', f_name='qr.json')
+    print(i_data.data)
+
+
+if __name__ == '__main__':
+    main()
